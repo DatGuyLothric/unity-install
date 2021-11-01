cd43d0c6a87b6fea1f80a54a798f3bfd0f20cd01
DatGuyLothric <denissteam1@mail.ru>
2021-11-01 08:08:55 +0300

    test

---
+++ check.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Inputs: platforms
+
+supported_platforms=('Windows' 'iOS' 'Android' 'Linux' 'WebGL' 'AppleTV')
+IFS='|' read -ra platforms <<< echo $1
+for i in "${ platforms[@] }"
+do
+  k=0
+  for j in "${ supported_platforms[@] }"
+  do
+    if [[ $i == $j ]]; then
+      echo "::set-output name=${ i,, }::'true'"
+      k=1
+    fi
+  done
+  if [[ k == 0 ]]; then
+    echo "$i not supported"
+    exit 1
+  fi
+done
+for i in "${ supported_platforms[@] }"
+do
+  if [[ !${ platforms[*] } =~ $i ]]; then
+    echo "::set-output name=${ i,, }::'false'"
+  fi
+done
